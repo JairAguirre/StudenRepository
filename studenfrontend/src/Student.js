6843148c6798e3ab91a2da0b58aa41294554e7dc
JairAguirre <aguirre.jair@gmail.com>
2021-12-05 00:30:40 -0500

    App Ended

--- studenfrontend/src/Student.js
+++ studenfrontend/src/Student.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
+import { makeStyles } from '@mui/styles';
 import TextField from '@mui/material/TextField';
 import { Container, Paper, Button} from '@mui/material';
-
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+              
+      },
+    },
+  }));
+  
 export default function Student() {
     const paperStyle={padding:'50px 20px', width:600,margin:"20px auto"}
     const[name,setName]=useState('')
     const[adress,setAdress]=useState('')
     const[students,setStudents]=useState([])
     const classes = useStyles();
+
     const handleClick=(e)=>{
         e.preventDefault()
         const student={name,adress}
@@ -22,6 +31,15 @@ export default function Student() {
     console.log("New Student added")
   })
     }
+
+    useEffect(()=>{
+        fetch("http://localhost:8080/student/getAll")
+        .then(res=>res.json())
+        .then((result)=>{
+          setStudents(result);
+        }
+      )
+      },[])
   return (
       <Container>
           <Paper elevation={3} style={paperStyle}>
@@ -46,9 +64,24 @@ export default function Student() {
 
   Submit
 </Button>
-{name}
-     {adress}
     </Box>
+    </Paper>
+    <h1>Students</h1>
+
+    <Paper elevation={3} style={paperStyle}>
+
+      {students.map(student=>(
+        <Paper elevation={6} style={{margin:"10px",padding:"15px", textAlign:"left"}} key={student.id}>
+         Id:{student.id}<br/>
+         Name:{student.name}<br/>
+         Address:{student.adress}
+
+        </Paper>
+      ))
+}
+
+
+
     </Paper>
     </Container>
   );
